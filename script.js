f31b8d186b47596cb3120f08162b52de744f123f
nathan <49448184+tatsuya98@users.noreply.github.com>
2025-07-26 14:15:04 +0100

    mobile search bar done

---
+++ script.js
@@ -0,0 +1,15 @@
+let map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
